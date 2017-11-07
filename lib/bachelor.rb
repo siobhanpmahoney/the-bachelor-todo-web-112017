9056cf81d5ea68a1a646b499e0826f90873f666f
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-11-07 20:12:54 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,12 +47,13 @@ end
 def get_average_age_for_season (data, season)
   age_array = []
   total_sum = ""
-  data.find do |season_no, season_info|
-    season_no == season
-    season_info.each do |i|
-      age_array << (i["age"]).to_i
+  data.each do |season_no, season_info|
+    if season_no == season
+      season_info.each do |i|
+        age_array << (i[:age]).to_i
+      end
     end
   end
   total_sum = age_array.inject(0) { |sum, i| sum + i }
-  (total_sum/age_array.length).to_f.round
+  (total_sum.to_f/age_array.length).to_f.round
 end
